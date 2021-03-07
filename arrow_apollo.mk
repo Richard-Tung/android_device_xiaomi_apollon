d1a33546f0384127be94a65328fceb692ea64a00
Richard Tung <dyq@dyq.me>
2021-03-07 22:56:18 +0800

    apollo: update kernel from apollo-user 11 RKQ1.200826.002 21.2.3 release-keys

--- arrow_apollo.mk
+++ arrow_apollo.mk
@@ -27,6 +27,6 @@ TARGET_INCLUDE_PIXEL_CHARGER := true
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="apollo-user 11 RKQ1.200826.002 20.12.25 release-keys"
+    PRIVATE_BUILD_DESC="apollo-user 11 RKQ1.200826.002 21.2.3 release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/apollopro_eea/apollo:10/QKQ1.200419.002/V12.0.13.0.QJDEUXM:user/release-keys
+BUILD_FINGERPRINT := qti/qssi/qssi:11/RKQ1.200826.002/21.2.3:user/release-keys
